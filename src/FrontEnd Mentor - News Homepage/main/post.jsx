d1f77e62faa39bc70eda7496e85afc3986a713c3
Akintade Japhet <oaaj.digitals@gmail.com>
2023-02-14 16:56:29 +0100

    Fix naming convention for new post Id

--- src/FrontEnd Mentor - News Homepage/main/post.jsx
+++ src/FrontEnd Mentor - News Homepage/main/post.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 
 const Post = ({ data }) => {
 	const [post, setPost] = useState(data);
+
 	return (
 		<React.Fragment>
-			<div className="new-post" id={post.title}>
+			<div className="new-post" id={`new-post-${post.id}`}>
 				<h4 className="new-post-title">{post.title}</h4>
 				<p className="new-post-des text">{post.body}</p>
 			</div>
