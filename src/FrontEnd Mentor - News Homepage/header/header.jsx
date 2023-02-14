d2569629a1842e7d31c6345f7ea77eb8a1b17621
Akintade Japhet <oaaj.digitals@gmail.com>
2023-02-14 22:00:57 +0100

    Responsive styling
    Add required html for the reponsive design
    Add appropriate css styles to elements to make  them more reponsive

--- src/FrontEnd Mentor - News Homepage/header/header.jsx
+++ src/FrontEnd Mentor - News Homepage/header/header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import logoImg from "../images/logo.svg";
 import NavItem from "./navItem";
 import "./header.css";
+import menuIcon from "../images/icon-menu.svg";
 
 const Header = () => {
 	const [navItems, setNavItems] = useState([
@@ -38,6 +39,15 @@ const Header = () => {
 				<img src={logoImg} alt="news logo" className="logo" />
 			</div>
 			<nav className="navbar">
+				<div className="menu-icon-div">
+					<img
+						src={menuIcon}
+						alt=""
+						className="menu-icon-img"
+						id="menu-icon"
+					/>
+				</div>
+				<div id="nav-list-mobile-bg"></div>
 				<ul className="nav-list" id="nav-list">
 					{navItems.map((navItem) => (
 						<NavItem key={navItem.id} data={navItem} />
